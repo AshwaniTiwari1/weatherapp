532c0cbc0470c6abe1a1219021df026568dc211b
AshwaniTiwari1 <60797963+AshwaniTiwari1@users.noreply.github.com>
2021-12-12 12:26:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
-//Get All Values
+
 const inputTxt = document.querySelector('.inputTxt');
 var button = document.querySelector('.btn');
 const showData = document.querySelector('.showData');
 
-//Get API Key By Login To OpenWeather.org
+
 const API_Key = "0cae6d5bed65de080cf62b03bebaf34b";
 
-//Now Add Event Listener
+
 button.addEventListener('click', () => {
 
-    //Get Input Value
+  
     const cityInput = inputTxt.value;
     // console.log(cityInput);
-
-    //Now Fetch Through Get API 
+    
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&APPID=${API_Key}`)
         .then(res => res.json())
         .then(data => {
             // console.log(data);
 
-            //When Fill Input Field Then Clear Input Field
+           
             inputTxt.value = " ";
 
-            //Now Show All Data Value
+          
             showData.innerHTML = `
                                 <ul>
                                     <li class="desc">${data.weather[0].description}</li>
